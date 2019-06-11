d3498b432013a369b95bc101a56c3523a08fb8e9
Nuno Pereira <nunocpereira@outlook.com>
2019-06-12 00:26:04 +0100

    svg color update

--- src/styles/Global.js
+++ src/styles/Global.js
@@ -6,23 +6,19 @@ const GlobalStyles = createGlobalStyle`
 
   @import url("https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,800,900");
 
-  body {
-    margin: 0;
-    padding: 0;
-  }
-
   html {
     box-sizing: border-box;
   }
 
-  *, *:before, *: after {
+  *, *: before, *: after {
     box-sizing: inherit;
   }
 
   body {
     ${custom};
+    margin: 0;
+    padding: 0;
   }
-
 `;
 
 export default GlobalStyles;
