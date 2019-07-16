55504a9e304809cb98774b0a19a2a28c81457198
NunoCPNP <nunocpereira@outlook.com>
2019-07-16 19:55:50 +0100

    react-waypoints

--- src/components/About/AboutText.js
+++ src/components/About/AboutText.js
@@ -1,50 +1,73 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useSpring, animated, config } from 'react-spring';
 import { Waypoint } from 'react-waypoint';
 
 import { yellow, below } from '../../utilities';
 
 import { Card } from '../../elements';
 
-const AboutText = () => (
-  <Wrapper>
-    <h2>
-    I don't Like to define myself by the
-      <span> work I have done </span>
-    . I define myself by the
-      <span> work I want to do </span>
-    . Skills can be taught, personality is inherent. I prefer to
-      <span> keep learning </span>
-    , to continue
-      <span> challenge </span>
-    myself, and to do interesting things that are meaningful.
-    </h2>
-    <SubGrid>
-      <h2>
-      I love to develop
-        <span> digital products </span>
-      that are :
-      </h2>
-      <AboutCard>
-        <h2>Fast</h2>
-        <h4>Fast load times and lag free interaction are my highest priority</h4>
-      </AboutCard>
-      <AboutCard>
-        <h2>Responsive</h2>
-        <h4>My layouts will work on any device, big or small</h4>
-      </AboutCard>
-      <AboutCard>
-        <h2>Intuitive</h2>
-        <h4>Strong preference for easy to use intuitive UX / UI</h4>
-      </AboutCard>
-      <AboutCard>
-        <h2>Dynamic</h2>
-        <h4>Websites don't have to be static. I love making things come to life</h4>
-      </AboutCard>
-    </SubGrid>
-  </Wrapper>
-);
+const AboutText = () => {
+  const [first, toggleFirst] = useState(false);
+
+  const firstAnimation = useSpring({
+    opacity: first ? 1 : 0,
+    transform: first ? 'translate3d(0, 0, 0)' : 'translate3d(-50%, 0, 0)',
+    config: config.gentle,
+  });
+
+  const secondAnimation = useSpring({
+    opacity: first ? 1 : 0,
+    transform: first ? 'translate3d(0, 0, 0)' : 'translate3d(-50%, 0, 0)',
+    config: config.slow,
+  });
+
+  return (
+    <Wrapper>
+      <animated.div style={firstAnimation}>
+        <Waypoint
+          bottomOffset="30%"
+          onEnter={() => { if (!first) toggleFirst(true); }}
+        />
+        <h2>
+        I don't Like to define myself by the
+          <span> work I have done </span>
+        . I define myself by the
+          <span> work I want to do </span>
+        . Skills can be taught, personality is inherent. I prefer to
+          <span> keep learning </span>
+        , to continue
+          <span> challenge </span>
+        myself, and to do interesting things that are meaningful.
+        </h2>
+      </animated.div>
+      <SubGrid>
+        <animated.h2 style={secondAnimation}>
+          I love to develop
+          <span> digital products </span>
+          that are :
+        </animated.h2>
+        <AboutCard>
+          <h2>Fast</h2>
+          <h4>Fast load times and lag free interaction are my highest priority</h4>
+        </AboutCard>
+        <AboutCard>
+          <h2>Responsive</h2>
+          <h4>My layouts will work on any device, big or small</h4>
+        </AboutCard>
+        <AboutCard>
+          <h2>Intuitive</h2>
+          <h4>Strong preference for easy to use intuitive UX / UI</h4>
+        </AboutCard>
+        <AboutCard>
+          <h2>Dynamic</h2>
+          <h4>Websites don't have to be static. I love making things come to life</h4>
+        </AboutCard>
+      </SubGrid>
+    </Wrapper>
+  );
+};
 
 export default AboutText;
 
--- src/elements/Card.js
+++ src/elements/Card.js
@@ -4,9 +4,11 @@ import styled from 'styled-components';
 import { lightBlue, below } from '../utilities';
 
 export const Card = styled.div`
-  background-color: ${lightBlue};
+  /* background-color: ${lightBlue}; */
   padding: 25px 20px;
   border-radius: 20px;
+  background-color: ${props => props.theme.colors.primary};
+  background-image: url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23766d85' fill-opacity='0.4' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E");
 
   ${below.xxl`
     padding: 25px 10px;
