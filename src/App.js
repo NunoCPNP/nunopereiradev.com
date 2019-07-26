f99e73c91d4877ceb1cb7cf6dddfb35832ba1189
NunoCPNP <nunocpereira@outlook.com>
2019-07-26 21:59:51 +0100

    small fix

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 // import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import { Helmet } from 'react-helmet';
 
 import GlobalStyle from './styles/Global';
 import HomePage from './pages/HomePage';
@@ -22,6 +23,10 @@ function App() {
   return (
     <>
       <Provider store={store}>
+        <Helmet>
+          <title>Nuno Pereira - Front End Developer</title>
+          <meta name="description" content="Nuno Pereira Front End Developer Portfolio" />
+        </Helmet>
         <HomePage />
       </Provider>
       <GlobalStyle />
